7db022e39d02a8583309ba00809c81171a65041e
Hzd0707 <67186253+Hzd0707@users.noreply.github.com>
2023-09-06 23:52:49 +0800

    Update reddit.js

--- Script/reddit.js
+++ Script/reddit.js
@@ -1,24 +1,3 @@
-let obj = JSON.parse($response.body);
-
-    obj.result.vip_expired_at = 4075798974,
-    obj.result.is_xy_vip = true,
-    obj.result.xy_vip_expire = 0,
-    obj.result.is_vip = true
-
-$done({body: JSON.stringify(obj)});
-#Surge #模块 #Reddit 过滤推广, 关 NSFW 提示
-小脚本小模块就不放 GitHub 了
-
-① reddit.sgmodule
-#!name=Reddit
-#!desc=过滤推广, 关 NSFW 提示 @xream
-
-[Script]
-Reddit = type=http-response,pattern=^https?:\/\/gql\.reddit\.com,requires-body=1,max-size=0,timeout=30,script-path=reddit.js
-
-[MITM]
-hostname = %APPEND%, gql.reddit.com
-② reddit.js
 let modified;
 let body;
 try {
@@ -48,4 +27,3 @@ try {
   $done(modified ? { body: JSON.stringify(body) } : {});
 }
 
-相关: 如何使用 Gist 创建私有脚本/模块
